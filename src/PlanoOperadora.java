e8622c56a12d1c5594251245c408e54bc0063535
teodoroast <adriano.teodoroast@gmail.com>
2022-11-06 12:17:27 -0300

    Rev-01/Plano

--- src/PlanoOperadora.java
+++ src/PlanoOperadora.java
@@ -1,9 +1,38 @@
 public class PlanoOperadora {
     public static void main(String[] args) {
-        //String plano 
+        /*  Planos:  T = Turbo ; M = Midia ; B = Basico */
+        String plano = "T";
+
+        switch (plano) {
+            case "T" : {
+                System.out.println("5Gb Youtube");
+            }
+            case "M" : {
+                System.out.println("WhatsApp e Instagram grátis");
+            }
+            case "B" : {
+                System.out.println("100 minutos de ligação");
+            }
+        }
     }
 }
 
 
 /* Se optar por switch / case, devo me aprofundar um 
-pouco mais em conceitos de continuidade, break e default.*/
+pouco mais em conceitos de continuidade, break e default.
+
+******************************************************************
+
+        Rev-01
+        String plano = "T";
+        switch (plano) {
+            case "T" : {
+                System.out.println("5Gb Youtube");
+            }
+            case "M" : {
+                System.out.println("WhatsApp e Instagram grátis");
+            }
+            case "B" : {
+                System.out.println("100 minutos de ligação");
+
+*/
