cca8c31001a82d1a1465a6250dc96a69954c4111
teodoroast <adriano.teodoroast@gmail.com>
2022-11-06 10:50:45 -0300

    CxEletro/ResEsc

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,26 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 5;
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+
+
+
+    }
+}
+/*  Primeiras linha de comandos...
+        int nota = 8;
+        if(nota >= 7)
+        System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova de Recuperação");
+        else
+        System.out.println("Reprovado"); 
+        
+        *******************************************************
+            Segunda linha de comandos...
+            int nota = 5;
+            String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+            System.out.println(resultado);
+        */
