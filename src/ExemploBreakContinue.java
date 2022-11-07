1bde45174c1768909a9b9a6dc2374475c6762231
teodoroast <adriano.teodoroast@gmail.com>
2022-11-07 18:17:22 -0300

    Exem-BreakCont

---
+++ src/ExemploBreakContinue.java
@@ -0,0 +1,28 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        /*Exemplo 02*/
+        for (int numero = 1; numero <=10; numero ++) {
+            if(numero == 9)
+                break; 
+
+                System.out.println(numero);
+        }
+    }
+}
+
+/**********************************************************
+    Exemplo 01
+    public static void main(String[] args) {
+        for (int numero = 1; numero <=5; numero ++) {
+            if(numero == 5)
+                break; 
+
+                System.out.println(numero);
+        }
+    }
+} 
+_____________________________________________________________
+OBS:_________________________________________________________ 
+     Pode-se notar que o podemos ver uma parada obrigatoria ou 
+     apenas uma alteração no processo. 
+************************************************************/
