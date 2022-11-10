ecca0d9f26bbe62b5fa79c410bce115eb84027fa
teodoroast <adriano.teodoroast@gmail.com>
2022-11-09 22:58:56 -0300

    Ordenação

--- src/Collection/ExemploOrdenacao.java
+++ src/Collection/ExemploOrdenacao.java
@@ -1,7 +1,70 @@
 package Collection;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ExemploOrdenacao {
     public static void main(String[] args) {
-        
+
+        List<Gato> meusGatos = new ArrayList<>(){{
+            add (new  Gato ("Jon" , 12 , "preto"));
+            add (new  Gato ("Simba" , 6 , "tigrado"));
+            add (new  Gato ("Jon" , 18 , "amarelo"));
+        }};
+
+        System.out.println("--\tOrdem de Inserção\t---");
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem aleatória\t---");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Natural (Nome)\t---");
+        Collections.sort(meusGatos);
+        System.out.println(meusGatos);
+
     }
+}
+
+class Gato implements Comparable<Gato> {
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public String getCor() {
+        return cor;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Gato gato) {
+        // TODO Auto-generated method stub
+        return this.getNome().compareToIgnoreCase(gato.getNome());
+    }
+    
+    
+        
+    
 }
